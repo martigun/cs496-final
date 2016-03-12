e8c5c43f995a3be67d863a00626b14c4fafe9cd0
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-03-12 12:48:24 -0500

    s

--- server.js
+++ server.js
@@ -18,10 +18,14 @@ app.get('/', function(req, res) {
 app.get('/myLoc', function(req, res) {
 
 
+	var testString = 'https://maps.googleapis.com/maps/api/directions/json?'
+	+ 'origin=33.7946333,-84.44877199999999&destination=30.25,-97.75'
+	+ '&key=AIzaSyBp8lIReveKnqn9vVuxHslFpJxv0Fj0stg';
+
 	var myKey = 'AIzaSyBp8lIReveKnqn9vVuxHslFpJxv0Fj0stg';
 	var qGeoString = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + myKey;
 
-	request.post(qGeoString, function(error, response, body){
+	request.post(testString, function(error, response, body){
 		
 		
 		console.log(body);
