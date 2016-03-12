e454f2d421097c5f3d798198f7d1cffdf9ef613f
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-03-12 10:20:35 -0500

    s

--- control.js
+++ control.js
@@ -6,8 +6,32 @@
 function getGeo(){
 	
 	
+	var qString = 'https://www.googleapis.com/geolocation'
+	qString += '/v1/geolocate?key=AIzaSyBp8lIReveKnqn9vVuxHslFpJxv0Fj0stg'
+	
 	alert("Get geo!");
 	
+	var req = new XMLHttpRequest();
+	req.open("POST", qString, true); //true for async!
+	
+	//waits for the response to load
+	req.addEventListener('load',function(){
+		//check status
+		if(req.status >= 200 && req.status < 400){
+			
+
+			alert("we got it!");
+			
+			
+		} else {
+			
+			//there was an error
+			console.log("Error in network request: " + req.statusText);
+		};
+	});
+	
+	//activates when call comes back
+	req.send(null);
 	
 }
 
