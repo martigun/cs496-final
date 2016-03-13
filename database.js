380e96329ac4d39eef17306f1bfc7d8c6e73ecc4
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-03-13 13:45:06 -0400

    s

--- database.js
+++ database.js
@@ -41,6 +41,279 @@ app.get('/player', function(req,res){
 	});	
 });
 
+// //GET for player with coach
+// app.get('/player_w_coach', function(req,res){
+	
+	// //message console
+	// console.log("I received a GET request for /player_w_coach");
+	
+	// var myArray = [];
+	
+	// //find all players
+	// db.player.find(function (err,docs) {	
+		
+		// //set nbr of players in collection
+		// var nbr_of_players = docs.length;
+		
+		// //myArray = docs;
+		
+		// for(var i=0; i < 5; i++){
+			
+			// myArray.push(docs[i]);
+			
+			// //var player_id = docs[i]._id;
+			
+			
+			// //console.log(docs[i].LastName);
+			// //console.log(docs[i]._id);
+			
+			// // var player_id = docs[i]._id;
+			
+			// // var coach_was_found = findMyCoach(player_id);
+			
+			// // var myStr = String(coach_was_found);
+			
+			// // console.log("From get: " + myStr);
+			
+			
+		// }
+		
+
+		
+		
+		// //res.json(docs); //sends data back
+		// //console.log("Number of players: " + nbr_of_players);
+		// //res.send("Number of players: " + nbr_of_players); //nice test
+		
+		
+		
+	// });	
+	
+	// // for(var i=0; i < 5; i++){
+	
+		// // var player_id = myArray[i]._id;
+		
+		// // myArray[i].MyCoach = findMyCoach(player_id);
+	
+	// // }
+	
+	// res.json(myArray);
+	
+	
+// });
+
+
+//GET a players coach 
+function findMyCoach(player_id){
+	
+	//see trace statments
+	var trace = 0;
+	
+	//get player to find from params
+	var player_to_find = mongojs.ObjectId(player_id); 
+
+	//look thru all coaches
+	db.coach.find(function (err,docs){
+		
+		//message server
+		console.log("P to find: " + player_to_find);
+		
+		//set nbr of coaches in collection
+		var nbr_of_coaches = docs.length;
+		if(trace) console.log("# of coaches: " + nbr_of_coaches);
+		
+		//set coachFound to zero
+		var coachFound = 0;
+		var coach_obj = {};
+		
+		//cycle thru the coaches
+		for(var i=0; i < nbr_of_coaches; i++){
+			
+			//get coach ID and # of players for each coach
+			var coach_id = docs[i]._id;
+			
+			var nbr_of_players = docs[i].Players.length;
+			
+			if(trace) console.log("C is now: " + coach_id);
+			if(trace) console.log("name: " + docs[i].UserName);
+			if(trace) console.log("# of players: " + nbr_of_players);
+			if(trace) console.log("--------------------------");
+			
+			//cycle thru this coach's players
+			for(var j=0; j < nbr_of_players; j++){
+				
+				//get player_id
+				var player_id = mongojs.ObjectId(docs[i].Players[j]);
+				
+				if(trace) console.log("#" + j + ": " + player_id);
+				
+				//convert both to strings
+				var p_find = String(player_to_find);
+				var p_curr = String(player_id);
+				
+				if(trace) console.log("p_curr type is: " + typeof(p_curr));
+				if(trace) console.log("p_find type is: " + typeof(p_find));
+				
+				//if current player matches the player to find
+				if(p_curr == p_find){
+					
+					//set coachFound to the found coach ID
+					coachFound = coach_id;
+					coach_obj = docs[i];
+					
+					if(trace) console.log(player_id + " EQUALS " + player_to_find);
+				}
+			};
+			if(trace) console.log("--------------------------");
+		};
+		
+		console.log("Coach found: " + coachFound);
+		//console.log("Coach name: " + coach_fname);
+		
+		
+		
+		//if coach was not found
+		if(coachFound != 0){
+			
+
+			console.log("Coach FirstName: " + coach_obj.FirstName);
+			console.log("Coach LastName: " + coach_obj.LastName);
+			
+			//return coach_fname
+		
+		} 
+		
+		return coachFound;
+		
+	});
+	
+	
+	
+	
+	
+
+	
+	
+	
+};
+
+
+//GET a players coach 
+app.get('/players_coach/:id', function(req,res){
+	
+	//message server
+	console.log("I received a GET request for /players_coach");
+	
+	//see trace statments
+	var trace = 0;
+	
+	//get player to find from params
+	var player_to_find = mongojs.ObjectId(req.params.id); 
+	if(trace) console.log("P to find: " + player_to_find);
+	
+	//find all coaches
+	db.coach.find(function (err,docs) {	
+		
+		//if there are no coaches
+		if(docs==null){
+			
+			res.status(404); //set status to nothing found
+			res.json(docs); //return null
+		
+		//if there are coaches
+		} else {
+			
+			//set nbr of coaches in collection
+			var nbr_of_coaches = docs.length;
+			
+			//set coachFound to zero
+			var coachFound = 0;
+			
+			//cycle thru the coaches
+			for(var i=0; i < nbr_of_coaches; i++){
+				
+				//get coach ID and # of players for each coach
+				var coach_id = docs[i]._id;
+				var nbr_of_players = docs[i].Players.length;
+				
+				if(trace) console.log("C is now: " + coach_id);
+				if(trace) console.log("name: " + docs[i].UserName);
+				if(trace) console.log("# of players: " + nbr_of_players);
+				if(trace) console.log("--------------------------");
+				
+				//cycle thru this coach's players
+				for(var j=0; j < nbr_of_players; j++){
+					
+					//get player_id
+					var player_id = mongojs.ObjectId(docs[i].Players[j]);
+					
+					if(trace) console.log("#" + j + ": " + player_id);
+					
+					//convert both to strings
+					var p_find = String(player_to_find);
+					var p_curr = String(player_id);
+					
+					if(trace) console.log("p_curr type is: " + typeof(p_curr));
+					if(trace) console.log("p_find type is: " + typeof(p_find));
+					
+					//if current player matches the player to find
+					if(p_curr == p_find){
+						
+						//set coachFound to the found coach ID
+						coachFound = coach_id;
+						if(trace) console.log(player_id + " EQUALS " + player_to_find);
+					}
+				};
+				if(trace) console.log("--------------------------");
+			};
+		};
+		
+		//messsage server
+		console.log("Coach value found:" + coachFound);
+		
+		//if coach was not found
+		if(coachFound == 0){
+			
+			//return null
+			res.json(null);
+		
+		//if coach was found
+		} else {
+			
+			//redirect to the coach callback
+			res.redirect('/coach/' + coachFound);
+		}
+	});	
+	
+	
+	
+});
+
+
+/*
+
+
+//------------------
+			db.coach.find().forEach(function(err2,docs2){
+				
+				//if the coach is not null...
+				if(docs2!=null){
+					
+					//remove this player_id
+					db.coach.findAndModify({ 
+						query: {_id: docs2._id},
+						update: {$pull: {
+							Players: player_id
+						}},
+						new: true,
+						}, function(err3,docs3){} //return nothing here
+					);
+				}
+			});
+//-------------------------------
+
+*/
+
 //GET for coach with id
 app.get('/coach/:id', function(req,res){
 	
@@ -52,6 +325,31 @@ app.get('/coach/:id', function(req,res){
 	
 	//find this coach
 	db.coach.findOne({_id:id},function (err,docs) {	
+		
+		//check to see if id was found
+		if(docs==null){
+			
+			res.status(404); //set status to nothing found
+			res.json(docs); //return null	
+		} else {
+			
+			res.json(docs); //sends data back
+		}
+	});	
+});
+
+//GET for coach by UserName
+app.get('/coach_by_username/:UserName', function(req,res){
+	
+	//set up id var using the param and convert
+	//var id = mongojs.ObjectId(req.params.id);
+	var thisUserName = req.params.UserName;
+	
+	//message console
+	console.log("I received a GET UserName request for /coach: " + thisUserName);
+	
+	//find this coach
+	db.coach.findOne({UserName:thisUserName},function (err,docs) {	
 		
 		//check to see if id was found
 		if(docs==null){
--- server.js
+++ server.js
@@ -18,32 +18,153 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //json data
 app.use(express.static(__dirname));
 
+//initiate session
+var session = require('express-session');
+app.use(session({secret:'Pockets1'}));
+
 //initiate handlebars
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-//var rootURL = 'http://localhost:3600'
-var rootURL = 'http://52.88.255.17/:3600'
+var rootURL = 'http://localhost:3600'
+//var rootURL = 'http://52.88.255.17:3600'
 
+//root//
 app.get('/', function(req, res) {
 	
-	//create context object
-	var obj = {};
+	//clear error status
+	req.session.errStatus = false;
 	
-	res.render('menu', obj);
+	//if not logged in...
+	var loggedIn = req.session.loggedIn | 0;
+	if(loggedIn == 0){ 
 	
-	//res.render('login', context);
-	//res.redirect('/viewCoaches');
-
+		//goto login
+		res.redirect('/login');
+	} else {
+	
+		//go to main menu
+		res.redirect('/menu');
+	}
 });
 
+//menu//
+app.get('/menu', function(req, res) {
+	
+	//create context object
+	var context = {};
+	
+	//set UserName and error status
+	context.loginUserName = req.session.loginUserName;
+	context.errStatus = req.session.errStatus;
+	context.UserID = req.session.UserID;
+	
+	//render the menu
+	res.render('menu',context);
+});
 
+//logout//
+app.get('/logout', function(req, res) {
 
+	//clear user and error status
+	req.session.errStatus = false;
+	req.session.loginUserName = false;
+	req.session.loggedIn = 0;
+	
+	//go to login
+	res.redirect('/login');
+});
+
+//login//
+app.get('/login', function(req, res) {
+	
+	//set error status
+	var context = {};
+	context.errStatus = req.session.errStatus;
+
+	//render page
+	res.render('login', context);
+});
+
+//checkLogin//
+app.get('/checkLogin', function(req, res) {
+	
+	//get the input UserName as password
+	var inputUserName = req.query.UserName;
+	var inputPassword = req.query.Password;
+	
+	//set options for search
+	var options = { 
+		method: 'GET',	
+		url : rootURL + '/coach_by_username/' + inputUserName
+	};
+	
+	//Find this user in the database
+	request(options, function(error, response, body){
+		
+		//set obj to the body
+		var obj = JSON.parse(body);
+		
+		//if username is not found...
+		if(!obj){
+			
+			//set error status and message console
+			console.log("User was not found: " + inputUserName);
+			req.session.errStatus = "UserName not found.";
+			
+			//go back to login
+			res.redirect('/login');
+			
+		//If UserName was found...
+		} else{
+			
+			console.log(obj);
+			
+			//set login user and pw
+			var foundUserName = obj.UserName;
+			var foundPassword = obj.Password;
+			var foundID = obj._id;
+			
+			
+			//message console
+			console.log("Found User: " + foundUserName);
+			console.log("Found Password: " + foundPassword);
+			console.log("Found ID: " + foundID);
+			
+			//if password is correct
+			if(inputPassword == foundPassword){
+				
+				//message console
+				console.log("Password is correct.");
+				
+				//set error status and UserName
+				req.session.errStatus = false;
+				req.session.loginUserName = foundUserName;
+				req.session.UserID = foundID;
+				req.session.loggedIn = 1;
+				
+				//go to main menu
+				res.redirect('/menu');
+			
+			//password is wrong
+			} else {
+				
+				//set error status and clear UserName
+				req.session.errStatus = "UserName and Password do not match.";
+				
+				//go back to login
+				res.redirect('/login');
+			}
+		}
+	});
+});
+
+//viewCoaches//
 app.get('/viewCoaches', function(req, res) {
 	
 	//create context object
-	var obj = {};
+	var context = {};
 
 	//Get coaches
 	var options = { method: 'GET',	url : rootURL + '/coach' };
@@ -53,13 +174,96 @@ app.get('/viewCoaches', function(req, res) {
 		var dArray = JSON.parse(body);
 		
 		//load up the object
-		obj.dArray = dArray;
+		context.dArray = dArray;
+		context.loginUserName = req.session.loginUserName;
 		
 		//render coaches
-		res.render('coaches', obj);
+		res.render('coaches', context);
 	});
 });
 
+//viewPlayers//
+app.get('/viewPlayers', function(req, res) {
+	
+	//create context object
+	var context = {};
+
+	//Get players
+	var options = { method: 'GET',	url : rootURL + '/player' };
+	request(options, function(error, response, body){
+		
+		//set dArray to the body
+		var dArray = JSON.parse(body);
+		
+		//load up the object
+		context.dArray = dArray;
+		context.loginUserName = req.session.loginUserName;
+		
+		//render players
+		res.render('players', context);
+	});
+});
+
+//viewPlayers//
+app.get('/viewOnePlayer', function(req, res) {
+	
+	//create context object
+	var context = {};
+	
+	//establish context
+	var fArray = ["player_id","FirstName","LastName","Pos","Team","ProjPts","Cost"]
+	
+	//fill context
+	for(var i=0; i < fArray.length; i++) context[fArray[i]] = req.query[fArray[i]];
+	context.loginUserName = req.session.loginUserName;
+	
+	//build URL
+	var getURL = rootURL + '/players_coach/' + req.query.player_id;
+
+	//Build options
+	var options = { 
+		method: 'GET',	
+		url : getURL  
+	};
+
+	//Find his coach
+	request(options, function(error, response, body){
+		
+		//set dArray to the body
+		var obj = JSON.parse(body);
+		
+		//If no coach
+		if(!obj){
+			
+			//render without coach
+			context.coach = null;
+			
+			//res.render('onePlayer',context);
+			console.log("No coach!");
+		
+		//if there is a coach
+		} else {
+			
+			//set context.coach to object
+			context.coach = obj;
+			//res.render('onePlayer',context);
+			
+			
+			console.log("Coach:" + obj.FirstName);
+		}
+		
+		
+		//render players
+		res.render('onePlayer',context);
+	});
+	
+	
+	
+	
+	
+});
+
+//deleteCoach//
 app.get('/deleteCoach', function(req, res) {
 	
 	//get coach id
@@ -78,21 +282,66 @@ app.get('/deleteCoach', function(req, res) {
 		console.log(body);
 		
 		//redirect to coach table
-		res.redirect('/viewCoaches');
+		req.session.loginUserName = false;
+		res.redirect('/login');
 	});
 });
 
-app.get('/addCoachForm', function(req, res) {
+//addCoachLoad//
+app.get('/addCoachLoad', function(req, res) {
 	
 	//render the addCoach page
 	var obj = {};
 	res.render('addCoach', obj);
 });
 
-app.get('/editCoachForm', function(req, res) {
+//addCoach//
+app.get('/addCoach', function(req, res) {
+	
+	var obj = {};
+	
+	console.log("Coach Add!");
+	
+	var origURL = req.originalUrl;
+	
+	console.log(origURL);
+	
+
+	
+	var options = {
+		method: 'POST',
+		url : rootURL + '/coach',
+		headers: {'Content-Type' : 'application/json'},
+		json: {
+			"UserName" : req.query.UserName,
+			"FirstName" : req.query.FirstName,
+			"LastName" : req.query.LastName,
+			"TeamName" : req.query.TeamName,
+			"Password" : req.query.Password,
+			"Players" : []
+		}
+	};
+	
+	request(options, function(error, response, body){
+		
+		
+		console.log(body);
+	});
+	
+	
+	
+	res.redirect('/');
+});
+
+//editCoachLoad//
+app.get('/editCoachLoad', function(req, res) {
+	
+	console.log(req.originalUrl);
 	
 	//get coach id
 	var coach_id = req.query.coach_id;
+	
+	console.log(coach_id);
 	
 	//set options
 	var options = { 
@@ -111,6 +360,7 @@ app.get('/editCoachForm', function(req, res) {
 	});
 });
 
+//editCoach//
 app.get('/editCoach', function(req, res) {
 	
 	//get coach id
@@ -148,60 +398,25 @@ app.get('/editCoach', function(req, res) {
 	});
 	
 	//back to coaches table
-	res.redirect('/viewCoaches');
+	req.session.errStatus = "Changes have been saved.";
+	res.redirect('/menu');
+	
 });
 
-
-
-app.get('/addCoach', function(req, res) {
-	
-	var obj = {};
-	
-	console.log("Coach Add!");
-	
-	var origURL = req.originalUrl;
-	
-	console.log(origURL);
-	
-
-	
-	var options = {
-		method: 'POST',
-		url : rootURL + '/coach',
-		headers: {'Content-Type' : 'application/json'},
-		json: {
-			"UserName" : req.query.UserName,
-			"FirstName" : req.query.FirstName,
-			"LastName" : req.query.LastName,
-			"TeamName" : req.query.TeamName,
-			"Password" : req.query.Password
-		}
-	};
-	
-	request(options, function(error, response, body){
-		
-		
-		console.log(body);
-	});
-	
-	
-	
-	res.redirect('/viewCoaches');
-});
-
-//open draft Distance page
-app.get('/draftDist', function(req, res) {
+//draftDistLoad//
+app.get('/draftDistLoad', function(req, res) {
 	
 	//create context object
-	var obj = {};
+	var context = {};
+	context.loginUserName = req.session.loginUserName;
 	
 	//render draft Dist
-	res.render('draftDist', obj);
+	res.render('draftDist', context);
 
 });
 
-//when Get Info is clicked
-app.get('/getDraft', function(req, res) {
+//draftDist//
+app.get('/draftDist', function(req, res) {
 	
 	
 	
@@ -246,46 +461,14 @@ app.get('/getDraft', function(req, res) {
 		context.draft_start = obj.routes[0].legs[0].start_address;
 		context.draft_end = obj.routes[0].legs[0].end_address;
 		
+		//get user name
+		context.loginUserName = req.session.loginUserName;
+		
 		//render page
 		res.render('draftDist', context);
 	});
 });
 
+//listen
 console.log("Running on 3700...")
-
 app.listen(3700);
-
-// // Archive---------------------------------------
-
-// app.get('/', function(req, res) {
-	
-	// //create context object
-	// var context = {};
-
-	// //set up options
-	// var options1 = {
-		// method: 'GET',
-		// url : rootURL + '/coach'
-	// };
-	
-	// var options2 = {
-		// method: 'POST',
-		// url : rootURL + '/coach',
-		// headers: {'Content-Type' : 'application/json'},
-		// json: {
-			// "UserName" : "bellban",
-			// "FirstName" : "Banny",
-			// "LastName" : "Bells"
-		// }
-	// };
-	
-	// request(options1, function(error, response, body){
-		
-		// console.log(body);
-		
-		
-	// });
-	
-	// res.render('login', context);
-
-// });
